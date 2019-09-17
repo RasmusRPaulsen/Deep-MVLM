696558a50efdebac8459ee9bfc53162c58a3156c
Rasmus R. Paulsen <rapa@dtu.dk>
2019-09-17 21:49:50 +0200

    transforms now generated externally

--- utils3d/render3d.py
+++ utils3d/render3d.py
@@ -254,6 +254,12 @@ class Render3D:
 
         return transform_stack
 
+    # if write_transform_files:
+    #   f = open(name_transform, 'w')
+    #    line = ' '.join(str(x) for x in np.array([rx, ry, rz, s, tx, ty]))
+    #    f.write(line)
+    #    f.close()
+
     def render_3d_obj_depth(self, transform_stack, file_name):
         slack = 5
         write_image_files = self.config['process_3d']['write_renderings']
@@ -387,19 +393,15 @@ class Render3D:
 
         return image_stack
 
-    def render_3d_obj_rgb(self, file_name):
-        # slack = 5
-
-        write_transform_files = False
+    def render_3d_obj_rgb(self, transform_stack, file_name):
         write_image_files = self.config['process_3d']['write_renderings']
         off_screen_rendering = self.config['process_3d']['off_screen_rendering']
         n_views = self.config['data_loader']['args']['n_views']
         img_size = self.config['data_loader']['args']['image_size']
-        winsize = img_size
+        win_size = img_size
 
         n_channels = 3
-        image_stack = np.zeros((n_views, winsize, winsize, n_channels), dtype=np.float32)
-        transform_stack = np.zeros((n_views, 6), dtype=np.float32)
+        image_stack = np.zeros((n_views, win_size, win_size, n_channels), dtype=np.float32)
 
         mtl_name = os.path.splitext(file_name)[0]+'.mtl'
         obj_dir = os.path.dirname(file_name)
@@ -420,7 +422,7 @@ class Render3D:
         # Initialize RenderWindow
         ren_win = vtk.vtkRenderWindow()
         ren_win.AddRenderer(ren)
-        ren_win.SetSize(winsize, winsize)
+        ren_win.SetSize(win_size, win_size)
         ren_win.SetOffScreenRendering(off_screen_rendering)
 
         obj_in.SetRenderWindow(ren_win)
@@ -437,65 +439,26 @@ class Render3D:
         while actor:
             actor.SetProperty(props)
             actor = actors.GetNextItem()
-
         del props
 
-        # Initialize Transform
         t = vtk.vtkTransform()
         t.Identity()
         t.Update()
 
-
-        # ttemp = vtk.vtkTransform()
-        # ttemp.Identity()
-
-        # Transform (assuming only one mesh)
-        # trans = vtk.vtkTransformPolyDataFilter()
-        # trans.SetInputConnection(obj_in.GetOutputPort())
-        # trans.SetTransform(ttemp)
-        # trans.Update()
-
-        #mappers = vtk.vtkPolyDataMapper()
-        #mappers.SetInputData(trans.GetOutput())
-
-        # actorText = vtk.vtkActor()
-        # actorText.SetMapper(mappers)
-        # actorText.SetTexture(texture)
-        # actorText.GetProperty().SetColor(1, 1, 1)
-        # actorText.GetProperty().SetAmbient(1.0)
-        # actorText.GetProperty().SetSpecular(0)
-        # actorText.GetProperty().SetDiffuse(0)
-
-        #actor_geometry = vtk.vtkActor()
-        #actor_geometry.SetMapper(mappers)
-        #actor_geometry.GetProperty().SetColor(1,1,1)
-        #actor_geometry.GetProperty().SetAmbient(1.0)
-        #actor_geometry.GetProperty().SetSpecular(0)
-        #actor_geometry.GetProperty().SetDiffuse(0)
-
-        # ren.AddActor(actorText)
-        #ren.AddActor(actor_geometry)
-
-        w2ifb = vtk.vtkWindowToImageFilter()
-        w2ifb.SetInput(ren_win)
+        w2if = vtk.vtkWindowToImageFilter()
+        w2if.SetInput(ren_win)
         writer_png = vtk.vtkPNGWriter()
-        writer_png.SetInputConnection(w2ifb.GetOutputPort())
+        writer_png.SetInputConnection(w2if.GetOutputPort())
 
         start = time.time()
         for idx in tqdm(range(n_views)):
             name_rendering = self.config.temp_dir / ('rendering' + str(idx) + '_RGB.png')
-            #        oname_depth = output_base + subject_name + "/" + faces + "_depth" + str(idx) + ".png"
-            #        oname_image = output_base + subject_name + "/" + faces + "_image" + str(idx) + ".png"
-            #        oname_LM = output_base + subject_name + "/" + faces + "_LMtrans" + str(idx) + ".txt"
-            name_transform = self.config.temp_dir / ('transform' + str(idx) + '.txt')
 
-            # Create random transform
-            rx, ry, rz, s, tx, ty = self.random_transform()
+            rx, ry, rz, s, tx, ty = transform_stack[idx]
             # rx,ry,rz,s,tx,ty = no_transform() # TODO debug
             # rx = -20
             # ry = 40
             # rz = 10
-            transform_stack[idx, :] = (rx, ry, rz, s, tx, ty)
 
             t.Identity()
             t.RotateY(ry)
@@ -503,36 +466,19 @@ class Render3D:
             t.RotateZ(rz)
             t.Update()
 
-            # trans.Update()
-            #       trans2.Update()
-
-            #    zmin = -150
-            #    zmax = 150
             xmin = -150
             xmax = 150
             ymin = -150
             ymax = 150
-
-            # Test if this works
-            # bounds = ren.ComputeVisiblePropBounds()
-            # zmin = trans.GetOutput().GetBounds()[4]
-            # zmax = trans.GetOutput().GetBounds()[5]
-            # slack = (zmax - zmin) / 2 #  TODO a clipping plane hack to ensure that view frustrum is big enough
-            #        xmin = trans.GetOutput().GetBounds()[0]
-            #        xmax= trans.GetOutput().GetBounds()[1]
-            #        ymin = trans.GetOutput().GetBounds()[2]
-            #        ymax= trans.GetOutput().GetBounds()[3]
             xlen = xmax - xmin
             ylen = ymax - ymin
-
-            # trans.Update()
 
             cx = 0
             cy = 0
             extend_factor = 1.0
             # The side length of the view frustrum which is rectangular since we use a parallel projection
             side_length = max([xlen, ylen]) * extend_factor
-            # zoom_factor = winsize / side_length
+            # zoom_factor = win_size / side_length
 
             ren.GetActiveCamera().SetParallelScale(side_length / 2)
             ren.GetActiveCamera().SetPosition(cx, cy, 500)
@@ -541,63 +487,20 @@ class Render3D:
             # TODO Clipping range computations should be redone when it is the camera that rotates instead of the actor
             # TODO only really important for depth rendering
             ren.GetActiveCamera().ApplyTransform(t.GetInverse())
-
-            #ren_win.Render()
-            #bounds = ren.ComputeVisiblePropBounds()
-            #zmin = bounds[4]
-            #zmax = bounds[5]
-            #ren.GetActiveCamera().SetClippingRange(500 - zmax - slack, 500 - zmin + slack)
             ren.ResetCameraClippingRange()  # TODO: This approach is not recommended when doing depth rendering
-            #           cur_range = ren.GetActiveCamera().GetClippingRange()
-            #            print(cur_range)
-            # ren_win.Render()
 
-            # Save textured image
-            #      actorText.SetVisibility(True)
-            # actorGeom.SetVisibility(false)
-            # renWin.Render()
-            # renWin.SetSize(winsize, winsize)
-            # renWin.Render()
-
-            # w2if = vtk.vtkWindowToImageFilter()
-            # w2if.SetInput(renWin)
-            # w2if.SetInputBufferTypeToRGB()
-
-            # writer = vtk.vtkPNGWriter()
-            # writer.SetInputConnection(w2if.GetOutputPort())
-            # writer.SetFileName(oname_image)
-            # writer.Write()
-
-            # Save depth
-            # w2if.SetInputBufferTypeToZBuffer()
-            # w2if.Update()
-
-            # scale = vtk.vtkImageShiftScale()
-            # scale.SetOutputScalarTypeToUnsignedChar()
-            # scale.SetInputConnection(w2if.GetOutputPort())
-            # scale.SetShift(0)
-            # scale.SetScale(-255)
-
-            # writer.SetInputConnection(scale.GetOutputPort())
-            # writer.SetFileName(oname_depth)
-            # writer.Write()
-            # del scale
-
-            # save geometry
-            #actor_geometry.SetVisibility(True)
-            # actorText.SetVisibility(False)
             ren_win.Render()
 
             if write_image_files:
-                w2ifb.Modified()  # Needed here else only first rendering is put to file
+                w2if.Modified()  # Needed here else only first rendering is put to file
                 writer_png.SetFileName(str(name_rendering))
                 writer_png.Write()
             else:
-                w2ifb.Modified()  # Needed here else only first rendering is put to file
-                w2ifb.Update()
+                w2if.Modified()  # Needed here else only first rendering is put to file
+                w2if.Update()
 
             # add rendering to image stack
-            im = w2ifb.GetOutput()
+            im = w2if.GetOutput()
             rows, cols, _ = im.GetDimensions()
             sc = im.GetPointData().GetScalars()
             a = vtk_to_numpy(sc)
@@ -605,25 +508,15 @@ class Render3D:
             a = a.reshape(rows, cols, components)
             a = np.flipud(a)
 
-            # For now just take the first channel
             image_stack[idx, :, :, :] = a[:, :, :]
 
-            if write_transform_files:
-                f = open(name_transform, 'w')
-                line = ' '.join(str(x) for x in np.array([rx, ry, rz, s, tx, ty]))
-                f.write(line)
-                f.close()
-
         end = time.time()
-        print("Pure rendering generation time: " + str(end - start))
+        print("Pure TGB rendering time: " + str(end - start))
 
-        del writer_png, w2ifb
-        #del trans, mappers, actor_geometry, ren, ren_win, t
+        del obj_in
+        del writer_png, w2if
         del ren, ren_win, t
-
-        #        del w2if, writer, w2ifB, writerB, trans, mappers, actorText, actorGeom, ren, renWin, T
-
-        return image_stack, transform_stack
+        return image_stack
 
     def render_3d_file(self, file_name):
         image_channels = self.config['data_loader']['args']['image_channels']
@@ -632,10 +525,15 @@ class Render3D:
         image_stack = None
         transformation_stack = None
         if file_type == ".obj" and image_channels == "RGB":
-            image_stack, transformation_stack = self.render_3d_obj_rgb(file_name)
+            transformation_stack = self.generate_3d_transformations()
+            image_stack = self.render_3d_obj_rgb(transformation_stack, file_name)
         elif file_type == ".obj" and image_channels == "depth":
             transformation_stack = self.generate_3d_transformations()
             image_stack = self.render_3d_obj_depth(transformation_stack, file_name)
+        elif file_type == ".obj" and image_channels == "RGB+depth":
+            transformation_stack = self.generate_3d_transformations()
+            image_stack_rgb = self.render_3d_obj_rgb(transformation_stack, file_name)
+            image_stack_depth = self.render_3d_obj_depth(transformation_stack, file_name)
         else:
             print("Can not render filetype ", file_type, " using image_channels ", image_channels)
 
